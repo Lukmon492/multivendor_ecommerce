d9759f732da34a159d131338a8657772737aadac
Lukmon492 <lukmon492@gmail.com>
2025-09-23 23:33:55 +0100

    shadcn-ui installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div>
       <h1 className="font-bold text-blue-500">Welcome to the course</h1>
+      <h1 className="font-bold text-blue-500 font-barlow">
+        Welcome to the course
+      </h1>
     </div>
   );
 }
