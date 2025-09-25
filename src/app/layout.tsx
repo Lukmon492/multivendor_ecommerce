29661eb81bebee2d21121e12bf585ea5349aae5f
Lukmon492 <lukmon492@gmail.com>
2025-09-25 11:31:31 +0100

    clerk conflict sorted and some stuffs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ import "./globals.css";
 // Theme Provider
 import { ThemeProvider } from "next-themes";
 
+// Clerk Provider
+import { ClerkProvider } from "@clerk/nextjs";
+
 // Fonts
 const interFont = Inter({ subsets: ["latin"] });
 const barlowFont = Barlow({
@@ -29,17 +32,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${interFont.className} ${barlowFont.variable}`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${interFont.className} ${barlowFont.variable}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import ThemeToggle from "@/components/shared/theme-toggle";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="p-5">
-      <div className="w-100 flex justify-end">
+      <div className="w-100 flex gap-x-5 justify-end">
+        <UserButton />
         <ThemeToggle />
       </div>
-      <h1 className="font-bold text-blue-500">Welcome to the course</h1>
-      <Button>Click me</Button>
+      <h1 className="font-bold text-blue-500">Home Page</h1>
     </div>
   );
 }
